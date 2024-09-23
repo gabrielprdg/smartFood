43cd95d4a0b409c69d48352d1e04b5d2d4cd3723
gabrielprdg <gabriel.pr07@hotmail.com>
2024-09-23 00:29:45 -0300

    feat: add AddOrderController

--- src/presentation/controllers/orders/add-order/add-order-controller.js
+++ src/presentation/controllers/orders/add-order/add-order-controller.js
@@ -1,4 +1,4 @@
-import { badRequest } from "../../../helpers/http/http-helper.js"
+import { badRequest, noContent, serverError } from "../../../helpers/http/http-helper.js"
 
 class AddOrderController {
   constructor(validation, addOrder) {
@@ -7,12 +7,18 @@ class AddOrderController {
   }
 
   async handle(httpRequest) {
-    const err = this.validation.validate(httpRequest.body)
+    try {
+      const err = this.validation.validate(httpRequest.body)
+      if (err) {
+        return badRequest(err)
+      }
 
-    if (err) {
-      return badRequest(err)
+      const { customerId, status, price, estimatedDate, createdAt } = httpRequest.body
+      await this.addOrder.add({ customerId, status, price, estimatedDate, createdAt })
+      return noContent()
+    } catch (err) {
+      return serverError(err)
     }
-
   }
 }
 
